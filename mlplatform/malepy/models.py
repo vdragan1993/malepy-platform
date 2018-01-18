4ecd106dec04f2cfb1c31c05b6dea41de903e364
vdragan1993 <vdragan1993@gmail.com>
2018-01-18 16:28:32 +0100

    Create models, update settings, update gitignore.

--- mlplatform/malepy/models.py
+++ mlplatform/malepy/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Model for Users, inheriting default Django User
+    """
+    user_role = models.IntegerField(null=False, blank=False, default=0)  # 0 = teacher, 1 = student
+    members = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Course(models.Model):
+    """
+    Model for Course
+    """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    shortcut = models.CharField(max_length=10, null=False, blank=False)
+    year = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)])
+    users = models.ManyToManyField(
+        User,
+        through='Enrollment',
+    )
+
+    def __str__(self):
+        return self.name + " " + str(self.year) + " (" + self.shortcut + ")"
+
+
+class Enrollment(models.Model):
+    """
+    Model for User's enrollment in Courses
+    """
+    user = models.ForeignKey(User, null=False, blank=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False, blank=False)
+    enrolled = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username + " enrolled in " + self.course.shortcut
+
+
+class Assignment(models.Model):
+    """
+    Model for Course Assignments
+    """
+    name = models.CharField(max_length=100, null=True, blank=True)
+    name_en = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=2000, null=True, blank=True)
+    description_en = models.CharField(max_length=2000, null=False, blank=False)
+    training = models.CharField(max_length=200, null=True, blank=True)
+    fake_testing = models.CharField(max_length=200, null=True, blank=True)
+    testing = models.CharField(max_length=200, null=True, blank=True)
+    starting = models.DateTimeField(null=True, blank=True)
+    ending = models.DateTimeField(null=True, blank=True)
+    folder = models.CharField(max_length=200, null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.course.shortcut + "  -  " + self.name_en
+
+
+class Submission(models.Model):
+    """
+    Model for Assignment's Submission
+    """
+    created = models.DateTimeField(auto_now=True, null=False, blank=False)
+    file_name = models.CharField(max_length=200, null=False, blank=False)
+    valid = models.BooleanField(null=False, blank=False, default=False)
+    result = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    approved = models.BooleanField(null=False, blank=False, default=True)
+    user = models.ForeignKey(User, null=False, blank=False)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return self.user.username + " for " + str(self.assignment) + " at " + str(self.created)
--- mlplatform/mlplatform/settings.py
+++ mlplatform/mlplatform/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'malepy.apps.MalepyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Belgrade'
 
 USE_I18N = True
 
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'malepy.User'
