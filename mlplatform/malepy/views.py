bc0eb210c89cf18cc5b87ae8274d54c4be643030
vdragan1993 <vdragan1993@gmail.com>
2018-01-19 00:46:28 +0100

    Introduce landing page.

--- mlplatform/malepy/views.py
+++ mlplatform/malepy/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    """
+    Homepage
+    """
+    return render(request, 'malepy/index.html')
+
