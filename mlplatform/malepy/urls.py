47e6589c283d8716dde232904a0e8d028b11fbd0
vdragan1993 <vdragan1993@gmail.com>
2018-01-19 14:32:57 +0100

    Update landing page. Introduce login page.

--- mlplatform/malepy/urls.py
+++ mlplatform/malepy/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'malepy'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^sign/$', views.sign_in, name='sign'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 ]
--- mlplatform/malepy/views.py
+++ mlplatform/malepy/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 def index(request):
@@ -7,3 +12,38 @@ def index(request):
     """
     return render(request, 'malepy/index.html')
 
+
+def sign_in(request):
+    """
+    Display Sign In form
+    """
+    return render(request, 'malepy/sign_in.html')
+
+
+def login(request):
+    """
+    User login
+    """
+    if request.method == 'POST':
+        # extract data
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # check for user
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                print("Vidi sta je i uloguj ga")
+            else:
+                context = {"message": "User account is not activated"}
+                return render(request, 'malepy/sign_in.html', context=context)
+        else:
+            context = {"message": "Wrong Username or Password"}
+            return render(request, 'malepy/sign_in.html', context=context)
+
+
+def logout(request):
+    """
+    User logout
+    """
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('malepy:index'))
